b8069ec162cdca55a00cf2c38599859541a01cba
Yavuz7 <yavuzyurtseven1@gmail.com>
2024-03-08 12:37:09 -0500

    Exercise 4.5 complete

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,14 +3,23 @@ const dummy = (blogs) => {
 };
 
 const totalLikes = (blogs) => {
-  const reducer = (sum, item) => {
+  const sumLikes = (sum, item) => {
     return sum + item.likes;
   };
 
-  return blogs.length == 1 ? blogs[0].likes : blogs.reduce(reducer, 0);
+  return blogs.length == 1 ? blogs[0].likes : blogs.reduce(sumLikes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  const mostLikes = (previous, next) => {
+    return previous.likes >= next.likes ? previous : next;
+  };
+
+  return blogs.reduce(mostLikes, { likes: 0 });
 };
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
