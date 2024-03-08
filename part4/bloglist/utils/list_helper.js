1b51ecce21e47882aa03fea5508331967cd9c522
Yavuz7 <yavuzyurtseven1@gmail.com>
2024-03-08 13:19:07 -0500

    Started 4.6

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -18,8 +18,29 @@ const favoriteBlog = (blogs) => {
   return blogs.reduce(mostLikes, { likes: 0 });
 };
 
+const mostBlogs = (blogs) => {
+  let highest = { author: "none", blogs: 0 };
+  const authors = [];
+  blogs.map((blog) => {
+    const currentAuthor = authors.find((author) => author.name === blog.author);
+    if (currentAuthor) {
+      currentAuthor.blogs += 1;
+      if (currentAuthor.blogs > highest.blogs) {
+        highest = currentAuthor;
+      }
+    } else {
+      authors.push({ author: blog.author, blogs: 1 });
+      if (highest.blogs == 0) {
+        highest = { author: blog.author, blogs: 1 };
+      }
+    }
+  });
+  return blogs.length == 1 ? { author: blogs[0].author, blogs: 1 } : authors;
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
