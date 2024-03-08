4ce5857cf49053d71d3df9f05089a2405e938677
Yavuz7 <yavuzyurtseven1@gmail.com>
2024-03-08 11:16:37 -0500

    4.2 seperated controller and blog model

--- part4/app.js
+++ part4/app.js
@@ -1,19 +1,13 @@
+const config = require("./utils/config");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mongoose = require("mongoose");
-const config = require("./utils/config");
+const blogRouter = require("./controllers/blogs");
 const logger = require("./utils/logger");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
+const mongoose = require("mongoose");
 
-const Blog = mongoose.model("Blog", blogSchema);
-
+mongoose.set("strictQuery", false);
 const mongoUrl = config.MONGODB_URI;
 mongoose
   .connect(mongoUrl)
@@ -27,18 +21,6 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogRouter);
 
 module.exports = app;
