f4ba9bf9b410b83c9cb4f980e6527bb6bbb4c188
Yavuz7 <yavuzyurtseven1@gmail.com>
2024-04-04 10:25:20 -0400

    Adding part 5

---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import LoginForm from "./components/loginForm";
+import blogService from "./services/blogs";
+
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <input value={newBlog} onChange={handleBlogChange} />
+      <button type="submit">save</button>
+    </form>
+  );
+
+  const addBlog = (e) => {};
+
+  const errorDisplay = () => <p>{errorMessage}</p>;
+
+  return (
+    <>
+      <h1>Login Here!</h1>
+      {errorMessage !== null && errorDisplay()}
+      {user === null ? (
+        <LoginForm setErrorMessage={setErrorMessage} setUser={setUser} />
+      ) : (
+        <div>
+          <p>{user.name} logged-in</p>
+          blogForm()
+        </div>
+      )}
+      <div>
+        <h2>blogs</h2>
+        {blogs.map((blog) => (
+          <Blog key={blog.id} blog={blog} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ part5/bloglist-frontend/src/components/LoginForm.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import loginServices from "../services/login";
+import blogServices from "../services/blogs";
+
+const LoginForm = ({ setErrorMessage, setUser }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginServices.login({
+        username,
+        password,
+      });
+      blogServices.setToken(user.token);
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+};
+export default LoginForm;
