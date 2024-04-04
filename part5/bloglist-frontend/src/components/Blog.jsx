c730e65e385646809a0ca88403708af902aefc1e
Yavuz7 <yavuzyurtseven1@gmail.com>
2024-04-04 12:45:05 -0400

    5.7 started

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,27 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const details = (
+    <div>
+      {blog.url}
+      <div>
+        {blog.likes}
+        <button>Like this post!</button>
+      </div>{" "}
+      {blog.author}
+    </div>
+  );
+  return (
+    <div>
+      {blog.title}
+      <button onClick={() => setVisible(!visible)}>
+        {!visible ? <div>View</div> : <div>close</div>}
+      </button>
+      {visible && details}{" "}
+    </div>
+  );
+};
+
+export default Blog;
